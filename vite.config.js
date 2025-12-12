079499b0eaeea879ee5d0f58aa08ce830bfd298b
Nunna anil sai <anilsainunna@gmail.com>
2025-12-12 15:48:23 +0530

    chang the path

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
-      },
-       base:"/"
+      }
     }),
     tailwindcss()
    
